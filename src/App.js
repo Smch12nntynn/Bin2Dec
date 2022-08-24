f9c6b7b2df620ae20fda592bee7bf01917124c82
michel marques <[200~you@example.comgit config --global user.email you@example.comgit config -- global user.email mixmarques99@gmail.com>
2022-08-24 17:50:26 -0300

    App ideas - Bind2Dec

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import videoBg from './matrixBg.mp4'
 
 function App() {
+  const [result, setResult] = useState();
+  const [errorMessage, setErrorMessage] = useState();
+  var verification = { isValid: true };
+
+  function handleChange(event) {
+    let number = event.target.value;
+    logicAplication(number);
+  }
+
+  function logicAplication(x) {
+    handleNumber(x);
+    erroMessenger();
+  }
+
+  // Making sure that we accept only either 0 or 1
+  function handleNumber(x) {
+    if (x.match(/^[0-1]+$/g) === null) {
+      verification.isValid = false;
+      return
+    }
+    verification.isValid = true;
+    formulate(x);
+  }
+
+  function erroMessenger() {
+    if (verification.isValid === false) {
+      setErrorMessage('Enter either 0 or 1')
+      setResult('') // Reset the result message
+    } else { setErrorMessage('') } // Reset the error message
+  }
+
+  // Formulae:
+  // input = 1 => output = 1 * (2^0) = 1
+  // input = 1010 => output = (0 * (2^0)) + (1 * (2^1)+ (0 * (2^2)+ (1 * (2^3)) = 10
+  function formulate(y) {
+    let reversedBinaryText = y
+      .split('')
+      .map(Number)
+      .reverse()
+    let result = reversedBinaryText.reduce((accumulator, currentValue, idx) =>
+      accumulator + currentValue * Math.pow(2, idx))
+    setResult(result)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <video src={videoBg} autoPlay loop muted />
+      <div className="content">
+        <h1>BIN2DEC</h1>
+        <form>
+          <label>
+            Insert Number :
+            <input type={"text"} onChange={handleChange} />
+          </label>
+        </form>
+        <h2>{errorMessage}</h2>
+        <h2>{result}</h2>
+      </div>
     </div>
   );
 }
